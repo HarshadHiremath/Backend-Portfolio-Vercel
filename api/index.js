ee21add7cc6c53a553145d3ddcf3c0d95cd066e2
“Harshad <harshadhiremath5@gmail.com>
2026-03-10 03:01:25 +0530

    Add

--- api/index.js
+++ api/index.js
@@ -3,27 +3,46 @@ import cors from "cors";
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
 // Test route
-app.get("/api", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "API working on Vercel 🚀"
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend is working 🚀"
   });
 });
 
-// Post test
-app.post("/api", (req, res) => {
+// POST route for Postman testing
+app.post("/api/test", (req, res) => {
+
   const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({
+      success: false,
+      message: "Name and Email required"
+    });
+  }
 
   res.json({
     success: true,
-    name,
-    email
+    message: "Data received successfully",
+    data: {
+      name,
+      email
+    }
   });
 });
 
-// ✅ IMPORTANT FOR VERCEL
+// Export for Vercel
 export default app;
+
+// Run locally
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
