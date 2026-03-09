a67dd4325dbaf2225215e8849b44e7e0cae74f00
“Harshad <harshadhiremath5@gmail.com>
2026-03-10 02:58:58 +0530

    Add

--- api/index.js
+++ api/index.js
@@ -1,49 +1,29 @@
 import express from "express";
-import serverless from "serverless-http";
 import cors from "cors";
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
 // Test route
-app.get("/", (req, res) => {
-  res.json({
-    message: "Backend is working 🚀"
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API working on Vercel 🚀"
   });
 });
 
-// POST route for Postman testing
-app.post("/api/test", (req, res) => {
-
+// Post test
+app.post("/api", (req, res) => {
   const { name, email } = req.body;
-
-  if (!name || !email) {
-    return res.status(400).json({
-      success: false,
-      message: "Name and Email required"
-    });
-  }
 
   res.json({
     success: true,
-    message: "Data received successfully",
-    data: {
-      name,
-      email
-    }
+    name,
+    email
   });
 });
 
-// Export for Vercel
-export default serverless(app);
-
-// Run locally
-if (process.env.NODE_ENV !== "production") {
-  const PORT = 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+// ✅ IMPORTANT FOR VERCEL
+export default app;
