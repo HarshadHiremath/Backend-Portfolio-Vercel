a51447397a30013fd2c2ec06aa6a7deda979b319
“Harshad <harshadhiremath5@gmail.com>
2026-03-10 03:52:31 +0530

    Created File Structure

---
+++ api/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import Status from "../views/status.html";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/* Root API */
+app.get("/api", (req, res) => {
+  res.sendFile(Status);
+});
+
+/* Test GET API */
+app.get("/api/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "GET API working"
+  });
+});
+
+/* Test POST API */
+app.post("/api/test", (req, res) => {
+  const { name, email } = req.body;
+
+  res.json({
+    success: true,
+    message: "POST API working",
+    data: {
+      name,
+      email
+    }
+  });
+});
+
+/* Export for Vercel */
+export default app;
+
+/* Localhost run */
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
